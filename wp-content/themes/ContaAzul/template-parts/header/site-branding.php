fc116ca13989d4bb5e01daf2d5f94512d089f3d3
cherobim22 <lucascherobim@hotmail.com>
2021-08-15 23:40:57 -0300

    update files

--- wp-content/themes/ContaAzul/template-parts/header/site-branding.php
+++ wp-content/themes/ContaAzul/template-parts/header/site-branding.php
@@ -6,5 +6,9 @@
 <?php if ( has_custom_logo()) : ?>
 	<div class="site-logo"><img class="footer-svg" src="<?php  echo get_template_directory_uri() . '/template-parts/header/imgs/logo_branca.png'?>" alt=""></div>
 <?php endif; ?>
-
+<style>
+	#masthead{
+		background-image: url(" <?php  echo get_template_directory_uri(). '/template-parts/header/imgs/hero.jpg' ?>");
+	}
+</style>
 
